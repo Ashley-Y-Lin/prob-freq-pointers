627c847871e12f9bad26fa33d9e9186d0858fa9a
Ashley Lin <ashleyylin11@gmail.com>
2023-04-07 15:49:33 -0700

    add averagePair

--- average-pair.js
+++ average-pair.js
@@ -1,7 +1,25 @@
-"use strict"
+"use strict";
 
-// add whatever parameters you deem necessary & write docstring
+/** averagePair finds whether there are two numbers in an array that create a
+ * target average. it takes as input an array of numbers, and an integer repsenting
+ * the target average */
+
 function averagePair(nums, targetAvg) {
+  let left = 0;
+  let right = nums.length - 1;
 
+  while (left < right) {
+    const avg = (nums[left] + nums[right]) / 2;
+
+    if (avg === targetAvg) {
+      return true;
+    } else if (avg > targetAvg) {
+      right -= 1;
+    } else {
+      left += 1;
+    }
+  }
+
+  return false;
 }
 

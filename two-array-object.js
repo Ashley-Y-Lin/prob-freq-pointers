6124954c2dfea1d95470e6b3ee97e1cad329340e
Ashley Lin <ashleyylin11@gmail.com>
2023-04-07 15:56:31 -0700

    add twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,15 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** twoArrayObjects makes an object created when taking one array as kets, and
+ * the other as values. it takes as input two arrays and returns an object */
+
+function twoArrayObject(keys, values) {
+  let obj = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    const curr = values[i] || null;
+    obj[keys[i]] = curr;
+  }
+
+  return obj;
 }
